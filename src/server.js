40114cbab7ae022e6b42f929518e2cfe96108e2d
JuunKR <bluejun0510@naver.com>
2022-07-18 05:58:43 +0900

    Feat: Add WebRTC, STUN

--- src/server.js
+++ src/server.js
@@ -15,4 +15,29 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
+function countRoom(roomName){
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size
+}
+
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName) => {
+      const count = countRoom(roomName) 
+      if ( count < 2 | count===undefined){
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+      }else{
+        socket.emit("limited")
+      }
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+      });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+      });
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+      });
+  });
+
 httpServer.listen(3000, handleListen);
